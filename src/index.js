454a80655049730648800ecdd0b38ebff0b11276
Bukola Adekoya <71440375+richbuky@users.noreply.github.com>
2023-12-20 13:04:45 +0100

    update on java

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+function updateWeather(response) {
+  let temperatureelement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let Cityelement = document.querySelector("#city");
+
+  Cityelement.innerHTML = response.data.city;
+  temperatureelement.innerHTML = Math.round(temperature);
+}
+
+function Searchcity(city) {
+  let apikey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function FormSubmit(event) {
   event.preventDefault();
   let SearchInput = document.querySelector("#search-form-input");
-  let Cityelement = document.querySelector("#city");
-  Cityelement.innerHTML = SearchInput.value;
+
+  Searchcity(SearchInput.value);
 }
 
 let Searchformelement = document.querySelector("#search-form");
